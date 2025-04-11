6aa22a7658dec438ec8ec966380a9f02b3622864
Divyani Sonawane <sonawanedivyani148@gmail.com>
2025-04-11 15:16:32 +0530

    deploy attempt 2

--- hkd/settings.py
+++ hkd/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,14 +78,7 @@ WSGI_APPLICATION = "hkd.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hkddb',
-        'USER': 'hkdadmin',
-        'PASSWORD': 'harekrishna',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 '''
