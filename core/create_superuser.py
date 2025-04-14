853997632460b1ce2d6690e4e629479350155569
Divyani Sonawane <sonawanedivyani148@gmail.com>
2025-04-14 12:49:06 +0530

    cred update

--- core/create_superuser.py
+++ core/create_superuser.py
@@ -8,9 +8,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SU_NAME", "admin")
-email = os.environ.get("DJANGO_SU_EMAIL", "admin@example.com")
-password = os.environ.get("DJANGO_SU_PASSWORD", "admin123")
+username = os.environ.get("DJANGO_SU_NAME", "hkdadmin")
+email = os.environ.get("DJANGO_SU_EMAIL", "sonawanedivyani148@gmail.com")
+password = os.environ.get("DJANGO_SU_PASSWORD", "HareKrishna@")
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
