d94e68118a1202b993e4adba338109f0da68c57d
Divyani Sonawane <sonawanedivyani148@gmail.com>
2025-04-14 17:08:28 +0530

    superuser change

--- core/create_superuser.py
+++ core/create_superuser.py
@@ -1,18 +1,26 @@
 import django
 from django.contrib.auth import get_user_model
-from django.conf import settings
+import sys
 import os
 
+# Add the parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Explicitly set the Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hkd.settings")
+
+# Now setup Django
 django.setup()
 
+# Get the user model
 User = get_user_model()
 
-username = "krishna"
+# Hardcoded superuser credentials
+username = "hkdadmin"
 email = "sonawanedivyani148@gmail.com"
 password = "harekrishna"
 
+# Check if the superuser already exists, if not, create one
 if not User.objects.filter(username=username).exists():
     print("👑 Creating superuser...")
     User.objects.create_superuser(username=username, email=email, password=password)
